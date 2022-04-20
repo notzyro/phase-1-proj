90904a763cc5df3556216f355b8da972be0244c4
notzyro <80290936+notzyro@users.noreply.github.com>
2022-04-20 14:59:24 -0400

    Start of filling in index.js function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Placeholder for index.js code
+function search() {
+  var request = document.getElementById('search_query').value;
+  
+  var userRequest = `https://api.jikan.moe/v4/anime?q=${request}&sfw`;
+  
+  document.getElementById('search_query').innerHTML = 'Searching...';
+  
+  fetch(userRequest)
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('search_query').innerHTML = 'Match found!';
+    
+    const maxResults = 4;
+    let i = 0;
+  }
+}
