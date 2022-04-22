8304ed8128085caa7b78cbb141aed3e108d24d48
notzyro <80290936+notzyro@users.noreply.github.com>
2022-04-22 06:24:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// Placeholder for index.js code
-function search() {
-  var request = document.getElementById('search_query').value; 
-  var userRequest = `https://api.jikan.moe/v4/anime?q=${request}&sfw`;  
+function search(sug) {
+  let request = document.getElementById('search_query').value;
+  if (request === '')
+    request = document.getElementById("search_query").placeholder;
+
+  let userRequest = `https://api.jikan.moe/v4/anime?q=${request}&sfw`;
 
   fetch(userRequest)
     .then(res => res.json())
@@ -10,31 +12,27 @@ function search() {
       document.getElementById('search_query').href = userRequest;
 
       var results = document.getElementById('search_results');
-      while (results.firstChild) {results.removeChild(results.firstChild);}
-
-      const maxResults = 10;
+      while (results.firstChild) { results.removeChild(results.firstChild); }
+      const maxResults = 5;
       let i = 1;
       try {
         data.data.forEach(item => {
-          if (i > maxResults) {
+          if (i > maxResults || i == sug) {
             throw BreakException;
           }
-
           document.getElementById('search_results')
-            .insertAdjacentHTML(
-              'beforeend',
+            .insertAdjacentHTML('beforeend',
               `
-                <a href="${item.url}" class="card">
-                    <div class="card__image">
-                        <img src="${item.images.jpg.small_image_url}" alt="${item.title}" />
-                    </div>
-                    <div class="card__name">
-                        <span>${item.title}</span> 
-                    </div>
-                </a>
+              <a href="${item.url}" class="card">
+              <div class="card__image">
+                  <img src="${item.images.jpg.large_image_url}" />
+              </div>
+              <div class="card__name">
+                <span>${item.title}</span> 
+              </div>
+              </a>
                 `
             );
-
           i++;
         });
       }
@@ -43,10 +41,15 @@ function search() {
       }
     });
 }
-var inputonEnter = document.getElementById('search_query');
 
-inputonEnter.addEventListener("keydown", function(event) {
-  if (event === 13) {
-      search();
+document.addEventListener('keydown', (e) => {
+  if (e.code === "Enter") {
+    search();
   }
-};
+});
+
+function randomize() {
+  let rand = Math.floor(Math.random() * suggestedAnime.length);
+  document.getElementById("search_query").placeholder = 'Suggested Anime: ' + suggestedAnime[rand];
+  search(2);
+}
