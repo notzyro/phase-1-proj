bec0177c17bf85e2b882bd75da165bee5520eb19
notzyro <80290936+notzyro@users.noreply.github.com>
2022-04-22 23:10:26 -0400

    Migrate functions to index.html
    Cleanup index.js and migrate main fetch to index.html

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
-function search(sug) {
-  let request = document.getElementById('search_query').value;
-  if (request === '')
-    request = document.getElementById("search_query").placeholder;
-
-  let userRequest = `https://api.jikan.moe/v4/anime?q=${request}&sfw`;
-
-  fetch(userRequest)
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById('search_query').innerHTML = userRequest;
-      document.getElementById('search_query').href = userRequest;
-
-      var results = document.getElementById('search_results');
-      while (results.firstChild) { results.removeChild(results.firstChild); }
-      const maxResults = 5;
-      let i = 1;
-      try {
-        data.data.forEach(item => {
-          if (i > maxResults || i == sug) {
-            throw BreakException;
-          }
-          document.getElementById('search_results')
-            .insertAdjacentHTML('beforeend',
-              `
-              <a href="${item.url}" class="card">
-              <div class="card__image">
-                  <img src="${item.images.jpg.large_image_url}" />
-              </div>
-              <div class="card__name">
-                <span>${item.title}</span> 
-              </div>
-              </a>
-                `
-            );
-          i++;
-        });
-      }
-      catch (e) {
-
-      }
-    });
-}
-
 document.addEventListener('keydown', (e) => {
   if (e.code === "Enter") {
-    search();
+    fetchData();
   }
 });
 
 function randomize() {
   let rand = Math.floor(Math.random() * suggestedAnime.length);
-  document.getElementById("search_query").placeholder = 'Suggested Anime: ' + suggestedAnime[rand];
-  search(2);
+  document.getElementById("search_query").placeholder = suggestedAnime[rand];
+  fetchData();
 }
